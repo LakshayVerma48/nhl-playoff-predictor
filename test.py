cb4fb4fec8a11f7319819661624daf0ac20d47c1
LakshayVerma48 <lakshay.v4@gmail.com>
2021-10-24 01:05:22 -0400

    Updated requirements

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Hello World!")
+import pandas as pd
+import requests
+import pandasql as pdsql
+
+response = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
+
+print(response.status_code)
