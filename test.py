a8c1247b07548a3abc2d17677e1538441164bacd
LakshayVerma48 <lakshay.v4@gmail.com>
2021-10-24 01:36:12 -0400

    Added team to start year mapping

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import pandas as pd
 import requests
 import pandasql as pdsql
+import matplotlib.pyplot as plt 
 
-response = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
 
-print(response.status_code)
+numTeams = 31
+teamNameAndStart = {}
+for i in range(1, numTeams):
+    response = requests.get("https://statsapi.web.nhl.com/api/v1/teams/" + str(i)).json()
+    yearOfStart = response['teams'][0]['firstYearOfPlay']
+    teamNameAndStart[response['teams'][0]['name']] = yearOfStart
+
+print(teamNameAndStart)
